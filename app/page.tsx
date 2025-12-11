7e8126f8067ae1048f12c10596343a56f03cb156
Gustavo Rizzo <140224461+guhrizzo@users.noreply.github.com>
2025-12-10 23:14:44 -0300

    add content

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,121 @@
-import Image from "next/image";
+import Footer from '../public/components/footer';
 
 export default function Home() {
   return (
     <div className="">
-      <p>Teste</p>
+      <div>
+        <h1>A Solução Financeira que Você Precisa. Simples. Transparente. Segura.
+        </h1>
+        <h2>Renegocie dívidas, recupere seu crédito e organize sua vida financeira com apoio profissional, atendimento humanizado e estratégias que realmente funcionam.
+        </h2>
+        <button>QUERO UMA CONSULTA GRATUITA
+        </button>
+        <ul>
+          <li>Especialistas em Crédito e Negociação</li>
+          <li>Atendimento Humanizado</li>
+          <li>Mais de X vidas transformadas</li>
+        </ul>
+      </div>
+      <div>
+        <h2>Quem é a Liberty CRED?
+        </h2>
+        <p>A Liberty CRED é uma empresa especializada em soluções financeiras inteligentes, atuando com ética, transparência e estratégia para ajudar pessoas a recuperarem crédito, reduzirem dívidas e tomarem decisões seguras.
+          Nosso objetivo é oferecer caminhos reais, simples e eficientes — sem burocracia, sem promessas vazias e com total segurança.
+        </p>
+        <h3>Pontos fortes!</h3>
+        <ul>
+          <li> Consultoria personalizada
+          </li>
+          <li> Processo seguro e amparado por legislação
+          </li>
+          <li>Equipe especializada
+          </li>
+          <li>Transparência total
+          </li>
+          <li>Alta taxa de sucesso
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2>NOSSOS SERVIÇOS</h2>
+        <h3>Soluções Financeiras que Transformam Sua Vida
+        </h3>
+        <ul>
+          <li>
+            <p> Limpeza de Nome (Amparo Jurídico Comercial)</p>
+            <p>Remoção de apontamentos indevidos em birôs como Serasa, SPC e Boa Vista, com base em leis como o Art. 42 do CDC.
+            </p>
+          </li>
+          <li>
+            <p> Negociação e Redução de Dívidas
+            </p>
+            <p>Avaliamos suas dívidas e buscamos acordos estratégicos com descontos reais.
+            </p>
+          </li>
+          <li>
+            <p> Consultoria de Crédito
+            </p>
+            <p>Orientação para melhorar seu score e recuperar seu acesso ao mercado de crédito.
+            </p>
+          </li>
+          <li>
+            <p> Intermediação Financeira
+            </p>
+            <p>Acompanhamos e orientamos processos envolvendo financiamentos, dívidas bancárias e regularização financeira.
+            </p>
+          </li>
+          <li>
+            <p>  Auxílio para Acesso a Crédito
+            </p>
+            <p>Preparamos você para conquistar limites maiores, crédito aprovado e vida financeira saudável.
+            </p>
+          </li>
+        </ul>
+        <button>Falar com um especialista</button>
+      </div>
+      <div>
+        <h2>COMO FUNCIONA O PROCESSO</h2>
+        <h3>Como é o passo a passo na Liberty CRED?</h3>
+
+        <p>Análise Gratuita</p>
+        <p>Você envia sua situação e fazemos um diagnóstico completo.</p>
+
+        <p>Estratégia Personalizada</p>
+        <p>Montamos um plano específico para o seu caso, com alternativas reais e seguras.</p>
+
+        <p>Execução</p>
+        <p>Damos andamento imediato no processo: negociação, acordos, pedidos de retirada, etc.</p>
+
+        <p>Resultado</p>
+        <p>Você recebe acompanhamento e vê sua situação mudar de verdade.</p>
+      </div>
+      <div>
+        <h2>POR QUE A LIBERTY CRED É DIFERENTE?</h2>
+        <h3>A diferença está no cuidado com você.</h3>
+        <ul>
+          <li>Atendimento humanizado</li>
+          <li>Zero enrolação e total clareza</li>
+          <li>Explicamos tudo que o mercado tenta esconder</li>
+          <li>Nada de taxas surpresa</li>
+          <li>Processo rápido e seguro</li>
+          <li>100% alinhado às leis do consumidor</li>
+          <li>Empresa que realmente luta pelo cliente</li>
+        </ul>
+      </div>
+      <div>
+        <h2>Resultados que falam por si</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quia sequi tempore nisi distinctio doloribus saepe non maiores dolores. Dolores quae veritatis nobis ipsam nostrum animi iste est tempore. Accusamus!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque incidunt repellat nesciunt ut sapiente dicta aliquid animi repellendus non autem tenetur voluptatem ducimus magnam officiis eos reiciendis, recusandae ratione consequuntur.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit eius quasi soluta, consequatur nemo adipisci at fuga quidem magni, mollitia unde molestiae, dolor magnam quaerat facilis sit enim maiores blanditiis.</p>
+      </div>
+      <div>
+        <h2>Sua mudança financeira começa agora.</h2>
+        <h3>Dê o primeiro passo. A análise é gratuita, segura e pode transformar sua vida.</h3>
+        <button>
+          QUERO MINHA CONSULTA GRATUITA
+        </button>
+      </div>
+      <Footer />
     </div>
   );
 }
