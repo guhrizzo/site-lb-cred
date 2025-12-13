6feb7c961654e87e9202c6ca12e8eb7f40a4edf2
Gustavo Rizzo <140224461+guhrizzo@users.noreply.github.com>
2025-12-13 00:56:12 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,26 +18,53 @@ export default function Home() {
         </div>
       </div>
       <div>
-        <h2>Quem é a Liberty CRED?
-        </h2>
-        <p>A Liberty CRED é uma empresa especializada em soluções financeiras inteligentes, atuando com ética, transparência e estratégia para ajudar pessoas a recuperarem crédito, reduzirem dívidas e tomarem decisões seguras.
-          Nosso objetivo é oferecer caminhos reais, simples e eficientes — sem burocracia, sem promessas vazias e com total segurança.
-        </p>
-        <h3>Pontos fortes!</h3>
-        <ul>
-          <li> Consultoria personalizada
-          </li>
-          <li> Processo seguro e amparado por legislação
-          </li>
-          <li>Equipe especializada
-          </li>
-          <li>Transparência total
-          </li>
-          <li>Alta taxa de sucesso
-          </li>
-        </ul>
-      </div>
-      <div>
+        <section data-aos="fade-up"
+          data-aos-duration="800"
+          data-aos-delay="100"
+          data-aos-once="true" className="bg-slate-100 py-20">
+          <div className="max-w-6xl mx-auto px-6">
+
+            <div className="border-l-4  border-green-500 pl-6">
+              <h2 className='text-4xl font-bold text-green-500'>Quem é a Liberty CRED?</h2>
+            </div>
+
+            <p className='pt-6 pb-6 text-xl'>A Liberty CRED é uma empresa especializada em soluções financeiras inteligentes, atuando com ética, transparência e estratégia para ajudar pessoas a recuperarem crédito, reduzirem dívidas e tomarem decisões seguras.
+              Nosso objetivo é oferecer caminhos reais, simples e eficientes — sem burocracia, sem promessas vazias e com total segurança.
+            </p>
+            <div className="border-l-4  border-green-500 pl-6">
+              <h2 className='text-4xl font-bold text-green-500 '>Pontos Fortes</h2>
+            </div>
+            <ul className='pt-6 text-xl flex flex-col'>
+              <li className='flex items-center gap-2'> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
+              </svg> Consultoria personalizada
+              </li>
+              <li className='flex items-center gap-2'> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M6.5 0A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0zm3 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z" />
+                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1A2.5 2.5 0 0 1 9.5 5h-3A2.5 2.5 0 0 1 4 2.5zm6.854 7.354-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708.708" />
+              </svg> Processo seguro e amparado por legislação
+              </li>
+              <li className='flex items-center gap-2'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
+              </svg>Equipe especializada
+              </li>
+              <li className='flex items-center gap-2'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M0 6.5a6.5 6.5 0 0 1 12.346-2.846 6.5 6.5 0 1 1-8.691 8.691A6.5 6.5 0 0 1 0 6.5m5.144 6.358a5.5 5.5 0 1 0 7.714-7.714 6.5 6.5 0 0 1-7.714 7.714m-.733-1.269q.546.226 1.144.33l-1.474-1.474q.104.597.33 1.144m2.614.386a5.5 5.5 0 0 0 1.173-.242L4.374 7.91a6 6 0 0 0-.296 1.118zm2.157-.672q.446-.25.838-.576L5.418 6.126a6 6 0 0 0-.587.826zm1.545-1.284q.325-.39.576-.837L6.953 4.83a6 6 0 0 0-.827.587l4.6 4.602Zm1.006-1.822q.183-.562.242-1.172L9.028 4.078q-.58.096-1.118.296l3.823 3.824Zm.186-2.642a5.5 5.5 0 0 0-.33-1.144 5.5 5.5 0 0 0-1.144-.33z" />
+              </svg>Transparência total
+              </li>
+              <li className='flex items-center gap-2'> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z" />
+                <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0" />
+              </svg>Alta taxa de sucesso
+              </li>
+            </ul>
+          </div>
+          <div>
+
+          </div>
+        </section>
+
         <h2>NOSSOS SERVIÇOS</h2>
         <h3>Soluções Financeiras que Transformam Sua Vida
         </h3>
